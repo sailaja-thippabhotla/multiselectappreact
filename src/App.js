27b8cf71b043d5042d466863f78d70f807fcc319
Sailaja Thippabhotla <49528989+sailaja-thippabhotla@users.noreply.github.com>
2021-02-24 13:47:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import DemoDropdown from './DemoDropdown'
+
+export default function App() {
+  return (
+    <div>
+      <DemoDropdown />
+    </div>
+  );
+}
